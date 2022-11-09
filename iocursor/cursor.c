5d57f9071dff019eb9852f253d0997a15bb131dd
Martin Larralde <martin.larralde@embl.de>
2022-11-09 10:52:04 +0100

    Fix `void*` cursor indexing in `cursor.c`

--- iocursor/cursor.c
+++ iocursor/cursor.c
@@ -376,8 +376,8 @@ iocursor_cursor_Cursor_readline_impl(cursor* self, Py_ssize_t size) {
     if (size == 0)
         return PyBytes_FromStringAndSize(NULL, 0);
 
-    void* start = (void*) &((char*) self->buffer.buf)[self->offset];
-    void* end   = memchr(start, '\n', size);
+    char* start = &((char*) self->buffer.buf)[self->offset];
+    char* end   = (char*) memchr(start, '\n', size);
 
     Py_ssize_t length = (end == NULL) ? size : end - start + 1;
     PyObject* bytes = PyBytes_FromStringAndSize((char*) start, length);
@@ -431,8 +431,8 @@ PyDoc_STRVAR(
 static inline PyObject*
 iocursor_cursor_Cursor_readlines_impl(cursor* self, Py_ssize_t hint) {
 
-    void*      start;
-    void*      end;
+    char*      start;
+    char*      end;
     PyObject*  bytes;
     PyObject*  lines;
     Py_ssize_t length = 0;
